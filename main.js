61f3ee0f4743f1fb1adeb33eb9eb36f574daa0f9
Wonjun Choi <wonjune12@hotmail.com>
2018-11-29 19:04:00 -0500

    added hover and footer

--- main.js
+++ main.js
@@ -2,8 +2,18 @@
 
 
 
-let buttonEvent = document.querySelector("#showProduct");
-
-buttonEvent.addEventListener("click",(event)=>{
+let productLineButton = document.querySelector("#showProduct");
+let hoverEvent = document.querySelectorAll("#item");
+productLineButton.addEventListener("click",(event)=>{
     document.getElementById("productLine").classList.toggle("show");
+})
+
+
+hoverEvent.addEventListener("click",(event)=>{
+    alert("The Item has been added to the cart");
+})
+
+let submit = document.querySelector("#email-sub");
+submit.addEventListener("click",(event)=>{
+    alert ("You have been subscribed to our newsletter!");
 })
